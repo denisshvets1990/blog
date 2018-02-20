8bfc474936199b764a81cd7adfeb7851ced1a3c0
denis <denis@denisNoote.localdomain>
2018-02-20 09:44:29 +0300

    remove unused code

--- app/helpers/grape/exceptions_handler_helper.rb
+++
@@ -1,9 +0,0 @@
-module Grape::ExceptionsHandlerHelper
-  extend ActiveSupport::Concern
-
-  included do
-    # rescue_from ActiveRecord::RecordNotFound do |e|
-    #  error!([e.message], 404)
-    # end
-  end
-end
